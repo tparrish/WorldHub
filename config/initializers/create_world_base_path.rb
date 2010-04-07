55f718f32d3da2d264255bc2c15a4c31bbc0fb1e
unknown <Danny.Tipple@.DubitLimited.local>
2010-04-07 11:00:36 +0100

    Misc changes ask matt asked

--- config/initializers/create_world_base_path.rb
+++ config/initializers/create_world_base_path.rb
@@ -1,3 +1,3 @@
-FileUtils.mkdir_p Configuration.world.base_path
-FileUtils.safe_unlink "public/world_assets"
-FileUtils.ln_s Configuration.world.base_path, "public/world_assets"
+#FileUtils.mkdir_p Configuration.world.base_path
+#FileUtils.safe_unlink "public/world_assets"
+#FileUtils.ln_s Configuration.world.base_path, "public/world_assets"
